7a3c64f3ae6ac7d10004a1deed5da27fb2a59e8a
Heric da Silva Cruz <hericsilvaho@gmail.com>
2023-08-01 11:02:43 -0300

    ExMon test

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,8 +1,54 @@
 defmodule ExMonTest do
   use ExUnit.Case
-  doctest ExMon
+  import ExUnit.CaptureIO
 
-  #test "greets the world" do
-   # assert ExMon.hello() == :world
-  #end
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %ExMon.Player{life: 100, name: "Heric", moves: %{move_heal: :heal, move_avg: :punch, move_rnd: :kick}}
+      assert expected_response == ExMon.create_player("Heric", :kick, :punch, :heal)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = ExMon.create_player("Heric", :kick, :punch, :heal)
+
+      messages = capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
+      assert messages =~ "The game is started"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = ExMon.create_player("Heric", :kick, :punch, :heal)
+      capture_io(fn ->
+      ExMon.start_game(player)
+      end)
+      :ok
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+
+
+      messages = capture_io(fn ->
+        ExMon.make_move(:punch)
+      end)
+
+      assert messages =~ "turn: :computer"
+      assert messages =~ "turn: :player"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages = capture_io(fn ->
+        ExMon.make_move(:invalid_move)
+      end)
+
+      assert messages =~ "invalid_move"
+    end
+  end
 end
